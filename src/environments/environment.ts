bd911ffd0a24ca21551e103902b0087650632cac
Dat Nguyen <datnguyen@novahub.vn>
2020-04-09 04:02:29 +0700

    ChangeAPI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl : 'https://helpdesk-kunlez-novahub.herokuapp.com/api'
+  apiUrl : 'https://hepldeskbackendntdat.herokuapp.com/api'
 };
 
 /*
